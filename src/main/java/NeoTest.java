e253955e8bca7895c22601fec136c842f1314421
Maxithistea <maximilian@von-unwerth.de>
2017-11-02 23:00:11 +0100

    First call of custom procedure.

--- src/main/java/NeoTest.java
+++ src/main/java/NeoTest.java
@@ -2,12 +2,30 @@ import org.neo4j.driver.v1.*;
 
 import static org.neo4j.driver.v1.Values.parameters;
 
+/**
+ * @author Maximilian von Unwerth
+ * Procedure aus dem Example Package wird aufgerufen.
+ */
 public class NeoTest {
     public static void main(String[] args) {
         Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("trump", "password"));
         Session session = driver.session();
+        StatementResult result = session.run("CALL example.search('T','title:*')",  //TODO Query isnt working
+                parameters("name", "Arthur"));
+        while (result.hasNext()) {
+            Record record = result.next();
+            System.out.println(record.get("title").asString() + " " + record.get("name").asString());
+        }
 
-        session.run("CREATE (a:Person {name: {name}, title: {title}})", parameters("name", "Arthur", "title", "King"));
+        session.close();
+        driver.close();
+    }
+
+    private static void createAndShow() {
+        Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("trump", "password"));
+        Session session = driver.session();
+
+        session.run("CREATE (a: Person {name: {name}, title: {title}})", parameters("name", "Arthur", "title", "King"));
 
         StatementResult result = session.run("MATCH (a:Person) WHERE a.name = {name} " +
                         "RETURN a.name AS name, a.title AS title",
