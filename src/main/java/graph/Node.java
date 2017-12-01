b3a6a4dae2e16c842b5cb1099c0ea81e45a9358c
fapse97 <fabian1597k@gmail.com>
2017-12-01 09:50:55 +0100

    2 neue Methoden bei Node hinzugefügt. Ermöglicht die visuelle Ausgabe eines Knotens mit allen verbundenen Knoten (eingehende+ausgehende Kanten)

--- src/main/java/graph/Node.java
+++ src/main/java/graph/Node.java
@@ -1,7 +1,6 @@
 package graph;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import static graph.Graph.nodes;
 
@@ -10,9 +9,9 @@ public class Node {
     //Bezeichnung des Knoten
     private String label;
     private String identifier;
-    //unsortierte Menge (HashSet) der ausgehenden Kanten
-    private ArrayList<Edge> incomingEdges = new ArrayList<>();
 
+    //Liste der ausgehenden bzw eingehenden Kanten
+    private ArrayList<Edge> incomingEdges = new ArrayList<>();
     private ArrayList<Edge> outgoingEdges = new ArrayList<>();
 
     public Node(String label, String identifier) {
@@ -25,16 +24,23 @@ public class Node {
         Node a = new Node("BasicNode", "A");
         Node b = new Node("BasicNode", "B");
         Node c = new Node("BasicNode", "C");
+        Node d = new Node("BasicNode", "D");
+        Node e = new Node("BasicNode", "E");
+        Node f = new Node("BasicNode", "F");
+        Node g = new Node("BasicNode", "G");
+        Node h = new Node("BasicNode", "H");
+        Node i = new Node("BasicNode", "I");
 
         a.addEdge(b, "Coole Verbindung von A nach B");
-        a.addEdge(c, "I bims 1 Kante vong A->C");
-        b.addEdge(c, "Verb. von B nach C");
-        System.out.println(Arrays.toString(a.incomingEdges.toArray()));
-        System.out.println(Arrays.toString(a.outgoingEdges.toArray()));
-        System.out.println(Arrays.toString(b.incomingEdges.toArray()));
-        System.out.println(Arrays.toString(b.outgoingEdges.toArray()));
-        System.out.println(Arrays.toString(c.incomingEdges.toArray()));
-        System.out.println(Arrays.toString(c.outgoingEdges.toArray()));
+        a.addEdge(d, "Kante");
+        e.addEdge(a, "Kante");
+        f.addEdge(a, "Test");
+        a.addEdge(g, "Kante");
+        a.addEdge(i, "Kante");
+        g.addEdge(f, "Kante");
+        g.addEdge(e, "Kante");
+        a.printNode();
+        f.printNode();
     }
 
     @Override
@@ -50,5 +56,134 @@ public class Node {
 
     public String getLabel() {
         return label;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    /*
+    *Baut ein Feld in dem die einzelnen Knoten und Kanten eingetragen werden.
+    *Gibt auf der Konsole einen Knoten mit allen verbundenen Knoten graphisch aus
+     */
+    public void printNode() {
+        String[][] field = new String[5][5];
+        field[2][2] = "[" + this.identifier + "]";
+        String right = " → ";
+        String left = " ← ";
+        String up = " ↑ ";
+        String down = " ↓ ";
+
+        //geht durch die Liste der ausgehenden Kanten
+        for (int x = 0; x < this.outgoingEdges.size(); x++) {
+            if (this.outgoingEdges.size() == 0) break;
+            if (field[2][3] == null) {
+                field[2][3] = right;
+                field[2][4] = "(" + outgoingEdges.get(x).getNode().get(1).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[3][3] == null) {
+                field[3][3] = " ↘ ";
+                field[4][4] = "(" + outgoingEdges.get(x).getNode().get(1).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[3][2] == null) {
+                field[3][2] = down;
+                field[4][2] = "(" + outgoingEdges.get(x).getNode().get(1).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[1][3] == null) {
+                field[1][3] = " ↗ ";
+                field[0][4] = "(" + outgoingEdges.get(x).getNode().get(1).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[2][1] == null) {
+                field[2][1] = left;
+                field[2][0] = "(" + outgoingEdges.get(x).getNode().get(1).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[1][1] == null) {
+                field[1][1] = " ↖ ";
+                field[0][0] = "(" + outgoingEdges.get(x).getNode().get(1).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[1][2] == null) {
+                field[1][2] = up;
+                field[0][2] = "(" + outgoingEdges.get(x).getNode().get(1).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[3][1] == null) {
+                field[3][1] = " ↙ ";
+                field[4][0] = "(" + outgoingEdges.get(x).getNode().get(1).getIdentifier().charAt(0) + ")";
+            }
+        }
+
+
+        //----------------------------------alle eingehenden Kanten-----------------------------------
+        for (int x = 0; x < this.incomingEdges.size(); x++) {
+            if (this.incomingEdges.size() == 0) break;
+            if (field[2][3] == null) {
+                field[2][3] = left;
+                field[2][4] = "(" + incomingEdges.get(x).getNode().get(0).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[3][3] == null) {
+                field[3][3] = " ↖ ";
+                field[4][4] = "(" + incomingEdges.get(x).getNode().get(0).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[3][2] == null) {
+                field[3][2] = up;
+                field[4][2] = "(" + incomingEdges.get(x).getNode().get(0).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[1][3] == null) {
+                field[1][3] = " ↙ ";
+                field[0][4] = "(" + incomingEdges.get(x).getNode().get(0).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[2][1] == null) {
+                field[2][1] = right;
+                field[2][0] = "(" + incomingEdges.get(x).getNode().get(0).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[1][1] == null) {
+                field[1][1] = " ↘ ";
+                field[0][0] = "(" + incomingEdges.get(x).getNode().get(0).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[1][2] == null) {
+                field[1][2] = down;
+                field[0][2] = "(" + incomingEdges.get(x).getNode().get(0).getIdentifier().charAt(0) + ")";
+                continue;
+            }
+            if (field[3][1] == null) {
+                field[3][1] = " ↗ ";
+                field[4][0] = "(" + incomingEdges.get(x).getNode().get(0).getIdentifier().charAt(0) + ")";
+            }
+        }
+        printArray(field);
+    }
+
+
+    /*
+    *gibt ein quadratisches Array auf der Konsole aus
+     */
+    private void printArray(String[][] array) {
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array.length; y++) {
+                try {
+                    if (array[x][y] != null) {
+                        System.out.print(array[x][y]);
+                    } else {
+                        System.out.print("   ");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Größe des Array muss quadratisch sein");
+                    break;
+                }
+            }
+            System.out.println();
+        }
     }
 }
