db7ee7308446b547df1074e5dd88290d14f4673c
fapse97 <fabian1597k@gmail.com>
2017-12-05 16:51:37 +0100

    erstellen des Graphen aus query sollte jetzt funktionieren; kommentare hinzugefügt; paar Ausgaben hinzugefügt die beim Debugging ggf. helfen

--- src/main/java/procedure/QueryBuilder.java
+++ src/main/java/procedure/QueryBuilder.java
@@ -19,65 +19,100 @@ public class QueryBuilder {
                 "RETURN m.title, d.name, collect(others.title) AS productions\n" +
                 "LIMIT 1";
         //query = "MATCH \"(:Actor {name: \"Wes Studi\"})-[:ACTS_IN]->(m:Movie)<-[:ACTS_IN]-(:Actor {name: \"Matt Gerald\"})";
+        /*query = "MATCH(thor:CHARACTER {name:'Thor'}),\n" +
+                "     (hulk:CHARACTER {name:'The Hulk'}),\n" +
+                "     (marvelMovies:TAG {name:'Marvel'}),\n" +
+                "     (newRelease:TAG {name:'New Release'})-[:HAS_TAG]-(movie)\n" +
+                "WHERE (marvelMovies)-[:HAS_TAG]-(movie)\n" +
+                "  AND (movie)-[:PERFORMS_IN]-(hulk)\n" +
+                "  AND (movie)-[:PERFORMS_IN]-(thor)\n" +
+                "RETURN movie.name"; */
         Graph g = build();
     }
 
+
+    /**
+     * Builds a graph from the given query
+     *
+     * @return the built graph
+     */
     public static Graph build() {
         Graph g = new Graph();
-        String label = "";
-        String identifier = "";
+
+        //not needed (same query preprocessing as in GraphProcedures)
+        String cleaned[] = query.split("RETURN");
+        String cleaned2[] = cleaned[0].split("WHERE");
+        String query = cleaned2[0];
+        System.out.println(query);
+
+        //vars
         String[] point = new String[2];
         String[] edge = new String[2];
         int direction = 0;
         Vertex first = null;
-        Vertex second = null;
+
+        //loop that goes through the whole query
         for (int x = 0; x < query.length(); x++) {
             String pointString = "";
             String edgeString = "";
+            if (query.charAt(x) == ',') first = null;
             if (query.charAt(x) == '>') direction = 1;
             if (query.charAt(x) == '<') direction = 2;
+
+            //'(' is an indicator for a (new) node in our graph
             if (query.charAt(x) == '(') {
                 x++;
+
+                //go through the query till the end of the node´s name
                 while (query.charAt(x) != ')' && query.charAt(x) != '{') {
                     pointString = pointString + query.charAt(x);
                     x++;
                 }
                 point = pointString.split(":");
                 Vertex n;
+
+                /*check for ':' and get the Label + Name of our node.
+                * Also checks if there is any label at all and creates a
+                * node with the label/name that is found
+                 */
                 if (pointString.contains(":")) {
                     if (g.checkLabel(point[0]) == null) {
                         n = new Vertex(point[0], point[1], g);
+                        System.out.println("Created Node! (" + point[0] + ":" + point[1] + ")");
                     } else if (g.checkLabel(point[0]).getLabel().equals("")) {
                         n = new Vertex(point[0], point[1], g);
+                        System.out.println("Created Node! (" + point[0] + ":" + point[1] + ")");
                     } else {
                         n = g.checkLabel(point[0]);
                     }
                 } else {
                     if (g.checkLabel(pointString) == null) {
                         n = new Vertex(pointString, "#", g);
+                        System.out.println("Created Node! (" + pointString + ":" + "#)");
                     } else {
                         n = g.checkLabel(pointString);
                     }
                 }
+
+                /*if a node is created earlier a new edge can be created depending
+                * on the edge parameters we find
+                 */
                 if (first != null) {
                     if (direction == 1) {
                         Edge e = new Edge(first, n, edge[1], g);
+                        System.out.println("Created Edge! (" + first.getLabel() + ":" + first.getIdentifier() + ")---" + edge[1] + "-->(" + n.getLabel() + ":" + n.getIdentifier() + ")");
                         first.addEdge(n, edge[1], g);
-                        //n.printVertex();
-                        //System.out.println(e.toString());
                     }
                     if (direction == 2) {
                         Edge e = new Edge(n, first, edge[1], g);
+                        System.out.println("Created Edge! (" + n.getLabel() + ":" + n.getIdentifier() + ")---" + edge[1] + "-->(" + first.getLabel() + ":" + first.getIdentifier() + ")");
                         n.addEdge(first, edge[1], g);
-                        //n.printVertex();
-                        //System.out.println(e.toString());
                     }
                 }
                 first = n;
-                //System.out.println(point[0]);
-                //System.out.println(point[1]);
             }
 
+            //find edge´s relation name
             if (query.charAt(x) == '[') {
                 x++;
                 while (query.charAt(x) != ']' && query.charAt(x) != '{') {
@@ -85,9 +120,10 @@ public class QueryBuilder {
                     x++;
                 }
                 edge = edgeString.split(":");
-                //System.out.println(edge[1]);
             }
         }
+
+        //prints all Points
         System.out.println("Alle Punkte:");
         for (Vertex v : Graph.vertices) {
             v.printVertex();
